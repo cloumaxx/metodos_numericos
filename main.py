6e81e2f3dd6226242b66b6b50097e182e6860ca8
CARLOSEDO-PC\Carlos Eduardo <alvearmutis@outlook.es>
2022-04-12 16:05:27 -0500

    listo para responder

---
+++ main.py
@@ -0,0 +1,27 @@
+import funciones.biseccion as bs
+import funciones.falsaposicion as Fb
+import funciones.calcBases as metodosBases
+import funciones.calcIEEE as cIeee
+import funciones.calcDerivadas as cD
+import funciones.calcPolinomiosRaices as cPl
+import funciones.newthon as cNewt
+import funciones.secante as cSec
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.optimize as rs
+import sympy as sp
+import math
+import sys
+import PyQt5
+from PyQt5.QtWidgets import *
+from interfazcalculadoras.menuPrincipal import control as cl
+from PyQt5 import QtGui
+
+
+def control():
+
+    cl()
+
+
+
+control()
